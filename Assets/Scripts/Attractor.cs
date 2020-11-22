164e6042b8be807cca2dd6b23fd10c201aabf461
Decoroader <secbittree@gmail.com>
2020-11-22 21:25:19 +0200

    closer to the original code by J. Bond

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    static public Vector3 POS = Vector3.zero; // a
+    [Header("Set in Inspector")]
+    public float radius = 8;
+    public float xPhase = 0.5f;
+    public float yPhase = 0.4f;
+    public float zPhase = 0.25f;
+
+    void FixedUpdate()
+    {
+        Vector3 tPos = Vector3.zero;
+        Vector3 scale = transform.localScale;
+        tPos.x = Mathf.Sin(xPhase * Time.time) * radius * scale.x;
+        tPos.y = Mathf.Sin(yPhase * Time.time) * radius * scale.y;
+        tPos.z = Mathf.Sin(zPhase * Time.time) * radius * scale.z;
+        transform.position = tPos;
+        POS = tPos;
+    }
+}
---
+++ Assets/Scripts/LookAtAttractor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class LookAtAttractor : MonoBehaviour
+{
+    void Update()
+    {
+         transform.LookAt(Attractor.POS);
+    }
+}
